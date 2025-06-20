490151b0a4d86c3df2fdf9c1daa90366ea618f41
Dan <dan@dan.com>
2025-06-20 18:32:55 +0300

    last commit

--- server/xpand/order_fulfiller.py
+++ server/xpand/order_fulfiller.py
@@ -9,6 +9,9 @@ from xpand.services import Robot
 from server.xpand.models.robotic_arm_motion import RoboticArmMotion
 from server.xpand.models.slot import Slot
 
+START = 1000
+END = 2000
+
 
 class OrderFulfiller:
 
@@ -34,29 +37,42 @@ class OrderFulfiller:
         possible_slots = items_to_possible_slots[item]
         for slot in possible_slots:
             current_position = await self.robot.get_position()
-            if not self.is_dangerous(slot, current_position):
+            start, end = self.get_dangerous_zone_start_end_x(
+                slot, current_position,
+            )
+
+            if not start and not end:
                 picked = await self._get_item_from_safe_slot(slot, item)
                 if picked:
                     return True
                 else:
                     continue
-            else:
-                pass
+            elif start:
+                await self._move_to_start_of_danger_while_folding(start, current_position[1])
+                if end:
+                    await self.robot.move_to(end, current_position[1])
+                    await self._move_to_destination_while_unfolding(slot)
 
-    def is_dangerous(self, slot_to_reach: Slot, current_position: tuple[int, int]) -> bool:
-        if slot_to_reach.x < 1000 and current_position[0] < 1000:
-            return False
-        elif slot_to_reach.x > 2000 and current_position[0] > 2000:
-            return False
-        elif slot_to_reach.x >= 1000 and slot_to_reach.x <= 2000:
-            return True
-        elif current_position[0] >= 1000 and slot_to_reach[0] <= 2000:
-            return True
-
-        return False
+    def get_dangerous_zone_start_end_x(self, slot_to_reach: Slot, current_position: tuple[int, int]):
+        if (slot_to_reach.x >= START and slot_to_reach.x <= END) or (current_position[0] >= START and slot_to_reach[0] <= END):
+            if (slot_to_reach.x >= START and slot_to_reach.x <= END) and (current_position[0] >= START and slot_to_reach[0] <= END):
+                return (current_position[0], None)
+            elif (slot_to_reach.x >= START and slot_to_reach.x <= END):
+                if current_position[0] < START:
+                    return (START, None)
+                elif current_position[0] > END:
+                    return (END, None)
+        elif slot_to_reach.x < START and current_position[0] > END:
+            return (END, START)
+        elif slot_to_reach.x > END and current_position[0] < START:
+            return (START, END)
+        return None, None
 
     async def _move_to_start_of_danger_while_folding(self, start_of_danger: tuple[int, int]):
         await asyncio.gather(self.robot.execute_motion(RoboticArmMotion.FOLD), self.robot.move_to(start_of_danger))
+
+    async def _move_to_destination_while_unfolding(self, destination: Slot):
+        await asyncio.gather(self.robot.move_to(destination.x, destination.y), self.robot.execute_motion(RoboticArmMotion.UNFOLD))
 
     async def _get_item_from_safe_slot(self, slot: Slot, item: str) -> bool:
         await self.robot.move_to(slot.x, slot.y)
