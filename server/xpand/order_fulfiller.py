caa617e44c6c8e595111a368a7c22f72516d157f
Dan <dan@dan.com>
2025-06-20 16:33:04 +0300

    ex-1 solution

--- server/xpand/order_fulfiller.py
+++ server/xpand/order_fulfiller.py
@@ -1,8 +1,12 @@
 from __future__ import annotations
 
+from typing import Set
+
 from xpand.models import Order
 from xpand.services import InventoryDb
 from xpand.services import Robot
+
+from server.xpand.models.slot import Slot
 
 
 class OrderFulfiller:
@@ -12,4 +16,11 @@ class OrderFulfiller:
         self.inventory = inventory
 
     async def fulfill_order(self, order: Order):
-        pass
+        slots = self._get_slots_by_order(order)
+
+    def _get_slots_by_order(self, order: Order) -> Set[Slot]:
+        all_slots = []
+        for item in order.items:
+            slots = self.inventory.find_slots_by_sku(item.sku)
+            all_slots.extend(slots)
+        return set(all_slots)
